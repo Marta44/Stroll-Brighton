88a285d9c8622ef132e4e0c1636013f8a3222ae2
Marta44 <martamarassi@hotmail.com>
2021-03-21 17:24:01 +0000

    Add input field for search locations in explore section

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,6 +1,6 @@
 function initMap (){
-    var map = new google.maps.map(document.getElementById("map"),{
-        zoom:5,
+    var map = new google.maps.Map(document.getElementById("map"),{
+        zoom:6,
         center: {
             lat: 50.827930,
             lng: -0.168749
