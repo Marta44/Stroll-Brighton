074ae981464cd103d5f673a49ae80ee7eac7ba3a
Marta44 <martamarassi@hotmail.com>
2021-03-21 16:41:03 +0000

    Create initMap function and css style to render it on the page

---
+++ assets/js/map.js
@@ -0,0 +1,9 @@
+function initMap (){
+    var map = new google.maps.map(document.getElementById("map"),{
+        zoom:5,
+        center: {
+            lat: 50.827930,
+            lng: -0.168749
+        }
+    });
+}
