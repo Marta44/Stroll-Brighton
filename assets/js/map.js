7f091168fc9403b094244becd07b332c27746b70
Marta44 <martamarassi@hotmail.com>
2021-03-22 16:10:19 +0000

    Add map.js and style the map in explore section

---
+++ assets/js/map.js
@@ -0,0 +1,9 @@
+function initMap (){
+    var map = new google.maps.Map(document.getElementById("map"),{
+        zoom:6,
+        center: {
+            lat: 50.827930,
+            lng: -0.168749
+        }
+    });
+}
