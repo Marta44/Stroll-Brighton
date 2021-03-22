11d95c738ad8cf5de17aafc1e9fa7e8006fa6b57
Marta44 <martamarassi@hotmail.com>
2021-03-22 19:22:45 +0000

    Add button type places in index.html

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,9 +1,11 @@
+//CREDIT: Course materials and Map centered to Brighton UK
 function initMap (){
     var map = new google.maps.Map(document.getElementById("map"),{
-        zoom:6,
+        zoom:7,
         center: {
             lat: 50.827930,
             lng: -0.168749
         }
     });
 }
+//CREDIT: Google Map Documentation: 
