33f4762de4d3d42d71c567e2211ff525e22070b6
Marta44 <martamarassi@hotmail.com>
2021-03-24 22:37:58 +0000

    Add zoom in on scroll event to hero image

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+/* Credit: jQuery plugin developed by Jason D'Oyley
+https://www.jqueryscript.net/zoom/zoom-image-scrolling.html
+*/
+var $window= $(window);
+$(window).scroll(function(event){
+    var scroll = $(window).scrollTop();
+    $(".heroImg-container img").css ({
+        width: (100 + scroll/5) + "%",
+    });
+});
