ed3b5f492a5868dfc512136688575392e7086919
Marta44 <martamarassi@hotmail.com>
2021-03-26 18:55:07 +0000

    Add sendMail function to js file

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,8 +1,19 @@
+// Credit: Code Institute course materials for emailJs
 function sendMail(contactForm){
     emailjs.send("Stroll-Brighton","CI-MS2",{
-        "from_name":contactForm.name.value,
+        "from_name":contactForm.fullname.value,
         "form_email":contactForm.email.value,
         "selection":contactForm.selection.value,
         "message_request":contactForm.message.value,
     })
+
+.then (
+    function (message){
+        alert("Message successfully sent!")
+    },
+    function (error){
+        alert("Error, Please fill the form again.")
+    });
+    document.getElementById("contactForm").reset(); // Clear the form fields after submission
+return false; // Stop loading a new page
 }
