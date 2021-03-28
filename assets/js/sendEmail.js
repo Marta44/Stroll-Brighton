d8010b76dc9b95ca6731c1428f8092256a14cba6
Marta44 <martamarassi@hotmail.com>
2021-03-28 13:56:33 +0000

    Change success or error messages in email.js

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -11,13 +11,13 @@ function sendMail(contactForm){
     // On success displays the browser alert window and the send button turns green
     function success(){
         let sendBtn = document.getElementById("send-btn");
-        sendBtn.style.backgroundColor="green";
+        sendBtn.innerHTML="Sent!";
         alert("Message successfully sent!")
     },
     // On error displays the browser alert window and the send button turns red
     function (error){
         let sendBtn = document.getElementById("send-btn");
-        sendBtn.style.backgroundColor="red";
+        sendBtn.innerHTML="Error!";
         alert("Error, Please fill the form again.", error)
     });
     document.getElementById("contactForm").reset(); // Clear the form fields after submission
