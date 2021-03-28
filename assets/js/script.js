9992458609510edbb5748beabb9b30eb5ba30960
Marta44 <martamarassi@hotmail.com>
2021-03-28 11:48:30 +0000

    Add carousel function

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,14 @@ $(window).scroll(function(event){
         width: (100 + scroll/5) + "%",
     });
 });
+
+// Carousel cards in tips section
+$(document).ready(function(){
+    $("#carouselArt").carousel();
+    $(".carousel-control-prev").click(function(){
+        $("#carouselArt").carousel("prev");
+    });
+    $(".carousel-control-next").click(function(){
+        $("#carouselArt").carousel("next");
+    });
+});
