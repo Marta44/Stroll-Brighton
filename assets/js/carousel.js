4f058638cb583c2f389d79250c656109753b95eb
Marta44 <martamarassi@hotmail.com>
2021-04-17 10:33:27 +0000

    Add modified wireframe of explore page and move place slider carousel in map.js

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,71 +1,39 @@
-/*CREDIT: to build this carousel I watched: https://www.youtube.com/watch?v=EG-sXgY1md4 
+/*CREDIT: to build the carousels I watched: https://www.youtube.com/watch?v=EG-sXgY1md4 
 and Owl Carousel documentation: https://owlcarousel2.github.io/OwlCarousel2/docs/api-classes.html
 */
 
-$(document).ready(function() {
-    let attractionSlider = $("#attraction-slider");
-
-    // Owl Carousel cards in tips section
-    attractionSlider.owlCarousel({
-        items: 4,
-        margin: 10,
-        nav: true,
-        navText: ['<i class="fas fa-chevron-left" aria-label= "left arrow">', '<i class="fas fa-chevron-right" aria-label= "right arrow"></i>'],
-        dotElement: ['<button role="button" aria-label="directional-dots" aria-selected="true"></button>'],
-        autoplay: false,
-        responsiveClass: true,
-        responsive:{
-            0:{
-                items: 1,
-                nav: true,
-                dots: false,
-            },
-            480:{
-                items: 2,
-                nav:true,
-                dots: false,
-            },
-            768:{
-                items: 3,
-                nav:true,
-            },
-            1024:{
-                items:4,
-                nav:true,
-            }
-        }
-    });
-});
+// Owl Carousel cards in tips section
+var attractionSlider = $("#attraction-slider");
 
 $(document).ready(function() {
-    let placesSlider = $("#card-places");
+	attractionSlider.owlCarousel({
+		items: 4,
+		margin: 10,
+		nav: true,
+		navText: ['<i class="fas fa-chevron-left" aria-label= "left arrow">', '<i class="fas fa-chevron-right" aria-label= "right arrow"></i>'],
+		dotElement: ['<button role="button" aria-label="directional-dots" aria-selected="true"></button>'],
+		autoplay: false,
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1,
+				nav: true,
+				dots: false,
+			},
+			480: {
+				items: 2,
+				nav: true,
+				dots: false,
+			},
+			768: {
+				items: 3,
+				nav: true,
+			},
+			1024: {
+				items: 4,
+				nav: true,
+			}
+		}
+	});
 
-    // Owl Carousel cards in explore section
-    placesSlider.owlCarousel({
-        items: 4,
-        margin: 10,
-        nav: false,
-        autoplay: false,
-        responsiveClass: true,
-        responsive:{
-            0:{
-                items: 1,
-                nav: true,
-                dots: false,
-            },
-            480:{
-                items: 2,
-                nav:true,
-                dots: false,
-            },
-            768:{
-                items: 3,
-                nav:true,
-            },
-            1024:{
-                items:4,
-                nav:true,
-            }
-        }
-    });
 });
